c55cb40cc0ccefafa83effe7ce37f9e1457cebd1
arjunphull123 <arjunphull@arizona.edu>
2023-09-21 12:41:10 -0700

    Implement generic search function

--- ASSIGNMENT/search.py
+++ ASSIGNMENT/search.py
@@ -73,8 +73,108 @@ def tinyMazeSearch(problem):
     w = Directions.WEST
     return [s, s, w, s, w, w, s, w]
 
+class Node:
+    """This class defines the structure of a node for use in the following
+    search functions. Each node has a state, a parent, an action, and a path
+    cost.
+    """
 
+    def __init__(self, state, parent=None, action=None, path_cost=0, heuristic_cost=0):
+        self.state = state
+        self.parent = parent
+        self.action = action
+        self.heuristic_cost = heuristic_cost
+        self.path_cost = path_cost
+        if self.parent:
+            self.path_cost += self.parent.path_cost
 
+    def get_path(self):
+        """Gets the path from the initial state to the current state.
+
+        :return: A list of actions representing the path from the initial state
+        to the current state.
+        """
+        path = []  # initialize path list
+        current_node = self  # set current node to expand
+        while current_node is not None:  # will traverse the entire path
+            if current_node.action is not None:  # if there is an action to add
+                path.append(current_node.action)  # add the action to the path
+            current_node = current_node.parent  # jump up one level to parent
+        path.reverse() # reverse the path
+        return path
+    
+
+    def in_frontier(self, frontier):
+        """Returns True if self.state is in frontier, False otherwise.
+
+        :param frontier: A Stack, Queue, or PriorityQueue.
+        :return: True if self.state is in frontier, False otherwise.
+        """
+        try:
+            for node in frontier.list:  # iterate across the frontier list
+                if node.state == self.state:  # compare states
+                    return True
+            return False
+        except AttributeError:
+            for (priority, count, item) in frontier.heap:  # iterate across the frontier heap
+                if item.state == self.state:  # compare states
+                    return True
+            return False
+
+def graphSearch(problem, frontier, heuristic=None):
+    """This function conducts a graph search for the given problem. The strategy used
+    is determined by the data structure of the given frontier, as follows:
+    - If the frontier is a Stack, the function uses depth-first-search.
+    - If the frontier is a Queue, the function uses breadth-first-search.
+    - If the frontier is a PriorityQueue, the function uses uniform-cost-search.
+    - If there is a heuristic function passed in, the function uses A* search.
+
+    :param problem: A search problem.
+    :param frontier: A Stack, Queue, or PriorityQueue.
+    :return: A list of actions that lead to the problem's goal state.
+    """
+    start = Node(problem.getStartState())
+
+    if problem.isGoalState(start.state):  # check for initial goal
+        return start.get_path()
+    
+    if heuristic:
+        start.heuristic_cost = heuristic(start.state, problem)  # get the heuristic cost of the node if using A*
+    
+    if isinstance(frontier, util.PriorityQueue):
+        frontier.push(start, start.path_cost + start.heuristic_cost)  # push the start node with the path cost (+ heuristic cost if using A*)
+    else:
+        frontier.push(start)  # push the start node to the frontier
+    explored = set()  # initialize explored set
+    
+    while not frontier.isEmpty():
+        node = frontier.pop()  # pop a node
+
+        if problem.isGoalState(node.state):  # goal-test
+            return node.get_path()
+        
+        if node.state not in explored:  # expand the node if it has not been explored
+            explored.add(node.state)  # add node to explored
+
+            successors = problem.getSuccessors(node.state)  # expand node
+            for successor in successors:
+                state = successor[0]
+                action = successor[1]
+                cost = successor[2]
+                child = Node(state, node, action, cost)  # create child node
+
+                if child.state not in explored:
+                    if isinstance(frontier, util.Stack):  # handle Stack
+                        frontier.push(child)
+                    elif isinstance(frontier, util.PriorityQueue):  # handle PriorityQueue
+                        if heuristic:  # handle A*
+                            child.heuristic_cost = heuristic(child.state, problem)
+                        frontier.update(child, child.path_cost + child.heuristic_cost)  # A* is the same as uniform cost, except heuristic augments the cost
+                    elif not child.in_frontier(frontier):  # handle Queue
+                        frontier.push(child)
+
+    # If no solution is returned:
+    util.raiseNotDefined()
 
 def depthFirstSearch(problem):
     """
@@ -92,95 +192,21 @@ def depthFirstSearch(problem):
     """
 
     "*** YOUR CODE HERE ***"
-    print("Start:", problem.getStartState())
-    print("Is the start a goal?", problem.isGoalState(problem.getStartState()))
-    print("Start's successors:", problem.getSuccessors(problem.getStartState()))
-    
-    # Create infrastructure to search:
-    class Node:
-        def __init__(self, state, parent=None, action=None):
-            self.state = state
-            self.parent = parent
-            self.action = action
-
-        def get_path(self):
-            """Gets the path from the initial state to the current state.
-
-            :return: _description_
-            """
-            path = []  # initialize path list
-            current_node = self  # set current node to expand
-            while current_node is not None:  # will traverse the entire path
-                if current_node.action:  # if there is an action to add
-                    path.append(current_node.action)  # add the action to the path
-                current_node = current_node.parent  # jump up one level to parent
-
-            # at this point, path is a path of actions from the current state to
-            # initial state. we need to reverse this
-
-            path.reverse() # reverse the path
-            return path
-
-    # Use the newly-minted Node class!
-    start = Node(problem.getStartState)  # start node is initalized with no parent or action
-
-    # If the start node is a goal state, return an empty list
-    if problem.isGoalState(start.state):
-        return start.get_path()
-    
-    # So, the start node is not a goal. Bummer! Let's search:
-
-    # Generic process is as follows: 
-    # First, initialize the frontier as a stack (LIFO). 
-    # Initialize an empty explored set.
-    # Push the start node to the frontier. 
-    
-    frontier = util.Stack()  # initialize frontier
-    explored = set()  # initialize explored set
-    frontier.push(start)  # push the start node to the frontier
-    
-    # Then, do the following until the frontier is empty (or loop breaks with return):
-    while not frontier.isEmpty():
-        # Choose a leaf and remove (pop) it from the frontier.
-        leaf = frontier.pop()
-        # If the leaf contains a goal state then return the corresponding solution
-        if problem.isGoalState(leaf.state):
-            return leaf.get_path()
-        # add the leaf (the node's state) to the explored set
-        explored.add(leaf.state)
-
-        # expand the chosen leaf, adding the resulting nodes to the frontier
-        # only if not in the frontier or explored set
-        # Expansion steps:
-        # 1. Get successors
-        successors = problem.getSuccessors(leaf.state)
-        # 2. Make nodes for each successor
-            # Note: successor is formatted as (state, action, stepCost)
-        for successor in successors:
-            state = successor[0]
-            action = successor[1]
-            succ_node = Node(state, action)
-            # 3. If the node is not in the frontier or the explored set, add it to the frontier
-            if succ_node.state not in explored and succ_node.state not in frontier.list:
-                frontier.push(succ_node)
-    # The gist of this is: the loop will repeat until a goal state is found or the frontier is empty.
-    
-    # If the code gets to this point, the frontier is empty
-    util.raiseNotDefined()
+    return graphSearch(problem, util.Stack())
 
 
 def breadthFirstSearch(problem):
     """Search the shallowest nodes in the search tree first."""
 
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    return graphSearch(problem, util.Queue())
 
 
 def uniformCostSearch(problem):
     """Search the node of least total cost first."""
 
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    return graphSearch(problem, util.PriorityQueue())
 
 
 def nullHeuristic(state, problem=None):
@@ -195,7 +221,7 @@ def aStarSearch(problem, heuristic=nullHeuristic):
     """Search the node that has the lowest combined cost and heuristic first."""
 
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    return graphSearch(problem, util.PriorityQueue(), heuristic)
 
 
 # Abbreviations
